8c7421257f30fa16f2ec17063e823dfb4f1f738e
Jonah Simpson <jonah.simpson@gmail.com>
2014-07-03 23:09:58 -0600

    initial basic commit

---
+++ WMIS/Controllers/TestApiController.cs
@@ -0,0 +1,14 @@
+﻿namespace WMIS.Controllers
+{
+	using System.Web.Http;
+
+	public class TestApiController : ApiController
+	{
+		[HttpGet]
+		[Route("HelloWorld/")]
+		public string GetHelloWorld()
+		{
+			return "Hello World";
+		}
+}
+}
