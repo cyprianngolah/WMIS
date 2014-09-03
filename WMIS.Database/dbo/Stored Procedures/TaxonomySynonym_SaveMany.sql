488afb90004cfb89b8f4b1bccd47f02f2ed08033
Jonah Simpson <jonah.simpson@gmail.com>
2014-09-03 02:23:39 -0600

    merge attempt 3

---
+++ WMIS.Database/dbo/Stored Procedures/TaxonomySynonym_SaveMany.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[TaxonomySynonym_SaveMany]
+	@p_taxonomyId INT,
+	@p_taxonomySynonyms TaxonomySynonymType READONLY
+AS
+	MERGE INTO 
+		[dbo].[TaxonomySynonyms] AS [target]
+	USING 
+		@p_taxonomySynonyms AS [source] ON [target].[TaxonomyId] = @p_taxonomyId AND [target].[Name] = [source].[Name]
+	WHEN NOT MATCHED BY TARGET 
+		THEN INSERT ([TaxonomyId], [Name]) VALUES (@p_taxonomyId, [source].[Name]) 
+	WHEN NOT MATCHED BY SOURCE 
+		AND [target].[TaxonomyId] = @p_taxonomyId 
+		THEN DELETE;
+
+RETURN 0
+GO
+
+GRANT EXECUTE ON [dbo].[TaxonomySynonym_SaveMany] TO [WMISUser]
+GO
