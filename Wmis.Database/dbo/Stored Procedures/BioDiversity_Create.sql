67a1e465ae9292bc0819864b4d874c9628e16c3a
jimmytwosocks <jmaltby@hotmail.com>
2014-09-16 15:04:35 -0700

    Removed Species Name constraint on stored proc

--- Wmis.Database/dbo/Stored Procedures/BioDiversity_Create.sql
+++ Wmis.Database/dbo/Stored Procedures/BioDiversity_Create.sql
@@ -1,11 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[BioDiversity_Create]
 	@p_name NVARCHAR(50)
 AS
-	IF EXISTS (SELECT 1 FROM dbo.Species WHERE Name = @p_name)
-	BEGIN
-		RAISERROR('Biodiversity Name already exists and must be unique.', 16, 1) WITH NOWAIT
-	END
-
 	INSERT INTO dbo.Species (Name)
 	VALUES (@p_name)
 
