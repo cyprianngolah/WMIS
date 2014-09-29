4a859d18c8e0714770c57d3455e1549119fc2751
Jonah Simpson <jonah.simpson@gmail.com>
2014-09-29 12:52:18 -0600

    fixing bug with dropdown binding on project screen

--- Wmis/js/wmis.knockout.js
+++ Wmis/js/wmis.knockout.js
@@ -15,7 +15,7 @@ $(function () {
                 $(element).select2(options);
             }, 0);
         },
-        update: function(element) {
+        update: function (element) {
             $(element).trigger('change');
         }
     };
@@ -80,46 +80,6 @@ $(function () {
     		$(element).datepicker('update', date);
     	}
 	};
-
-    //ko.bindingHandlers.datepicker = {
-    //	init: function (element, valueAccessor, allBindingsAccessor) {
-    //		//initialize datepicker with some optional options
-    //		var options = allBindingsAccessor().datepickerOptions || {};
-    //		$(element).datepicker(options);
-
-    //		//when a user changes the date, update the view model
-    //		ko.utils.registerEventHandler(element, "changeDate", function (event) {
-    //			var value = valueAccessor();
-    //			if (ko.isObservable(value)) {
-    //				value(event.date);
-    //			}
-    //		});
-
-    //		ko.utils.registerEventHandler(element, "change", function () {
-    //			var value = valueAccessor();
-    //			if (ko.isObservable(value)) {
-    //				value(new Date(element.value));
-    //			}
-    //		});
-    //	},
-    //	update: function (element, valueAccessor) {
-    //		var widget = $(element).data("datepicker");
-    //		//when the view model is updated, update the widget
-    //		if (widget) {
-    //			widget.date = ko.utils.unwrapObservable(valueAccessor());
-
-    //			if (!widget.date) {
-    //				return;
-    //			}
-
-    //			if (_.isString(widget.date)) {
-    //				widget.date = new Date(widget.date);
-    //			}
-
-    //			widget.setValue();
-    //		}
-    //	}
-    //};
 
     ko.extenders.async = function (computedDeferred, initialValue) {
 
