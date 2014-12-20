d76d42319f11fb1be66b0027914c727c3a5d3834
Jonah Simpson <jonah.simpson@gmail.com>
2014-12-19 21:07:21 -0700

    changed deploy script to have a configurable option for blocking deploy on possible data loss; made Person.JobTitle nullable because it was preventing deploy and that seemed more logical than trying to come up with a default value

--- Wmis.Database/dbo/Tables/Person.sql
+++ Wmis.Database/dbo/Tables/Person.sql
@@ -3,6 +3,6 @@
 	[PersonId] INT NOT NULL IDENTITY , 
     [Name] NVARCHAR(50) NOT NULL, 
     [Email] NVARCHAR(50) NOT NULL, 
-    [JobTitle] NVARCHAR(50) NOT NULL, 
+    [JobTitle] NVARCHAR(50) NULL, 
     CONSTRAINT [PK_Person] PRIMARY KEY ([PersonId])
 )
