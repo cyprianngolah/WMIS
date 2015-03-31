90e2e919c00bb113f3abcb662813e7e4a8dea4a5
Michael Johnson <michael.johnson@makeit.com>
2015-03-31 14:36:02 -0700

    Added Lat, Long and Timestamp and columns by default

--- Wmis.Database/dbo/Stored Procedures/SurveyTemplate_Save.sql
+++ Wmis.Database/dbo/Stored Procedures/SurveyTemplate_Save.sql
@@ -1,7 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[SurveyTemplate_Save]
 	@p_surveyTemplateId INT = NULL,
 	@p_name NVARCHAR(50),
-	@p_createdBy NVARCHAR(50)
+	@p_createdBy NVARCHAR(50),
+	@newRowId int = NULL
 AS
 	IF EXISTS (SELECT 1 FROM dbo.SurveyTemplate WHERE Name = @p_name AND SurveyTemplateId != ISNULL(@p_surveyTemplateId, SurveyTemplateId))
 	BEGIN
@@ -14,9 +15,26 @@ AS
 		INSERT INTO 
 			dbo.SurveyTemplate (Name, CreatedBy)
 		VALUES
-			(@p_name, @p_createdBy)		
+			(@p_name, @p_createdBy);
 
-		SELECT SCOPE_IDENTITY()
+		SET @newRowId = SCOPE_IDENTITY();
+
+		INSERT INTO 
+			dbo.SurveyTemplateColumns (SurveyTemplateId, IsRequired, Name, SurveyTemplateColumnTypeId, [Order])
+		VALUES
+			(@newRowId, 1, 'Latitude', (SELECT [SurveyTemplateColumnTypeId] FROM dbo.SurveyTemplateColumnTypes WHERE [Name] = 'Numeric'), 0);
+
+		INSERT INTO 
+			dbo.SurveyTemplateColumns (SurveyTemplateId, IsRequired, Name, SurveyTemplateColumnTypeId, [Order])
+		VALUES
+			(@newRowId, 1, 'Longitude', (SELECT [SurveyTemplateColumnTypeId] FROM dbo.SurveyTemplateColumnTypes WHERE [Name] = 'Numeric'), 1);
+			
+		INSERT INTO 
+			dbo.SurveyTemplateColumns (SurveyTemplateId, IsRequired, Name, SurveyTemplateColumnTypeId, [Order])
+		VALUES
+			(@newRowId, 1, 'Timestamp', (SELECT [SurveyTemplateColumnTypeId] FROM dbo.SurveyTemplateColumnTypes WHERE [Name] = 'Timestamp'), 2);
+
+		SELECT @newRowId;
 	END
 	ELSE
 	BEGIN
