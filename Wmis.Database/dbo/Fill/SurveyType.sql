2ecff56b73a9ce6096d7815c0056b1fc16e42694
James Maltby <James.Maltby@makeit.com>
2015-10-05 13:34:00 -0700

    forgot check on SurveyType

--- Wmis.Database/dbo/Fill/SurveyType.sql
+++ Wmis.Database/dbo/Fill/SurveyType.sql
@@ -1,4 +1,4 @@
-﻿IF EXISTS ( SELECT * FROM [dbo].[SurveyType] WHERE [SurveyTypeId] NOT IN (1,2,3,4,5,6,7,8) )
+﻿IF EXISTS ( SELECT * FROM [dbo].[SurveyType] WHERE [SurveyTypeId] NOT IN (1,2,3,4,5,6,7,8,9) )
 BEGIN
 	; THROW 51000, 'Records found in [dbo].[SurveyType]] other than what exists in the Fill Script.', 1; 
 END
