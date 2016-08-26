30d2b6adffac88b01e70d7b0ff11ee7b085adec3
James Maltby <James.Maltby@makeit.com>
2016-08-26 15:17:25 -0700

    add herd histroy table.

--- Wmis.Database/dbo/Tables/CollaredAnimalHerdAssociationHistory.sql
+++ Wmis.Database/dbo/Tables/CollaredAnimalHerdAssociationHistory.sql
@@ -6,8 +6,10 @@
     [HerdAssociationMethodId] INT NULL, 
     [HerdAssociationDate] DATETIME NULL, 
     [UserId] INT NULL, 
+    [CollaredAnimalId] INT NULL, 
     CONSTRAINT [PK_CollaredHerdId] PRIMARY KEY CLUSTERED ([CollaredAnimalHerdId]),
 	CONSTRAINT [FK_CollaredAnimalHerdHistory_ConfidenceLevels_HerdAssociation] FOREIGN KEY ([HerdAssociationConfidenceLevelId]) REFERENCES [dbo].[ConfidenceLevels] ([ConfidenceLevelId]),
 	CONSTRAINT [FK_CollaredAnimalHerdHistory_HerdPopulations] FOREIGN KEY ([HerdPopulationId]) REFERENCES [dbo].[HerdPopulations] ([HerdPopulationId]),
 	CONSTRAINT [FK_CollaredAnimalHerdHistory_HerdAssociationMethods] FOREIGN KEY ([HerdAssociationMethodId]) REFERENCES [dbo].[HerdAssociationMethods] ([HerdAssociationMethodId]),
+	CONSTRAINT [FK_CollaredAnimalHerdHistory_CollaredAnimals] FOREIGN KEY([CollaredAnimalId])REFERENCES [dbo].[CollaredAnimals] ([CollaredAnimalId]),
 )
