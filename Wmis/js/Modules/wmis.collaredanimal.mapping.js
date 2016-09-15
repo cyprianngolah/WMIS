87d5a9a60cf2f3ac8e2d9e6455447ab90af8ad5b
James Maltby <James.Maltby@makeit.com>
2016-09-15 14:08:45 -0700

    change moment function call to excluded UTC.)

--- Wmis/js/Modules/wmis.collaredanimal.mapping.js
+++ Wmis/js/Modules/wmis.collaredanimal.mapping.js
@@ -141,7 +141,7 @@ wmis.collaredanimal.mapping = (function ($) {
                 {
                     "data": "locationDate",
                     "render": function(data, type, row) {
-                        var date = moment.utc(data, moment.ISO_8601).local().format('L h:mm a');
+                        var date = moment(data, moment.ISO_8601).local().format('L h:mm a');
                         return date;
                     }
                 },
