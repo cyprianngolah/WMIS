30430062cd93b9571e0e3bb3732bea36f342a003
James Maltby <James.Maltby@makeit.com>
2017-04-19 11:55:54 -0700

    Removed Unique constraint on Name in Taxonomy table.

--- Wmis.Database/dbo/Tables/Taxonomy.sql
+++ Wmis.Database/dbo/Tables/Taxonomy.sql
@@ -3,6 +3,5 @@
 	[TaxonomyGroupId]   INT             NOT NULL,
 	[Name]              NVARCHAR (50)   NOT NULL,
     CONSTRAINT [PK_Taxonomy] PRIMARY KEY CLUSTERED ([TaxonomyId]),
-    CONSTRAINT [UK_Taxonomy_Name] UNIQUE ([Name]),
     CONSTRAINT [FK_Taxonomy_TaxonomyGroups] FOREIGN KEY ([TaxonomyGroupId]) REFERENCES [dbo].[TaxonomyGroups] ([TaxonomyGroupId])
 )
