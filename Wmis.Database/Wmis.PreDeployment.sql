f160da566ca57788ddac581f2c75d33a425ca652
James Maltby <James.Maltby@makeit.com>
2017-04-19 11:36:56 -0700

    don't drop tables on pre deployment.

--- Wmis.Database/Wmis.PreDeployment.sql
+++ Wmis.Database/Wmis.PreDeployment.sql
@@ -2,10 +2,5 @@
 Pre-Deployment Script 
 */
 
-alter table Person add column JobTitle NVARCHAR(50) NULL;
-update Person set JobTitle = 'Job Title';
-alter table Person alter column JobTitle NVARCHAR(50) NOT NULL;
 
-drop table dbo.CollarHistory;
-drop table dbo.Collars;
-drop table dbo.ArgosPasses;
+
