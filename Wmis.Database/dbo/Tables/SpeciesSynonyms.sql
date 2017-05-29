fac91e6778c10e3650ab34195f5122a022d1a930
James Maltby <James.Maltby@makeit.com>
2017-05-29 15:45:41 -0700

    Manual change to Prod table...Adjusted in code to fix issue.

--- Wmis.Database/dbo/Tables/SpeciesSynonyms.sql
+++ Wmis.Database/dbo/Tables/SpeciesSynonyms.sql
@@ -2,7 +2,7 @@
     [SpeciesSynonymId]        INT             NOT NULL IDENTITY,
 	[SpeciesSynonymTypeId]    INT             NOT NULL,
 	[SpeciesId]               INT             NOT NULL,
-	[Name]                    NVARCHAR (50)   NOT NULL,
+	[Name]                    VARCHAR(200)   NULL,
     CONSTRAINT [PK_SpeciesSynonyms] PRIMARY KEY CLUSTERED ([SpeciesSynonymId]),
     CONSTRAINT [UK_SpeciesSynonyms_SpeciesSynonymTypeId_SpeciesId_Name] UNIQUE ([SpeciesSynonymTypeId], [SpeciesId], [Name]),
     CONSTRAINT [FK_SpeciesSynonyms_SpeciesSynonymTypeId] FOREIGN KEY ([SpeciesSynonymTypeId]) REFERENCES [dbo].[SpeciesSynonymTypes] ([SpeciesSynonymTypeId]),
