e81fefe63427d1be745e1f00e92494fbe278e666
Neba Funwi-gabga <fgneba@gmail.com>
2018-07-24 18:51:08 -0400

    Turned off flagging locations with greater than 5 days

--- Wmis/Logic/ArgosJobService.cs
+++ Wmis/Logic/ArgosJobService.cs
@@ -418,7 +418,7 @@
 
                             // Neba: add another logic to check if difference in days from last location is > 5
                             var daysDifference = span.TotalDays;
-
+                            /*
                             if (daysDifference > 5)
                             {
                                 var comment = pass.Comment;
@@ -430,7 +430,7 @@
                                 _repository.CollarUpdateWarning(collar.Key, collarWarningState.Key, "Location Status", string.Format("Value reported on {0:MM/dd/yyyy} at {1:h:mm:ss tt}", pass.LocationDate, pass.LocationDate), badPassStatus.Name);
 
                                 _repository.ArgosPassUpdate(pass.Key, badPassStatus.Key, comment);
-                            }
+                            }*/
                         }
 
                         lastLocationDate = pass.LocationDate;
