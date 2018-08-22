7ba48eb7f488ca102f692f0e4de82a7410c4cf66
Neba Funwi-gabga <fgneba@gmail.com>
2018-08-22 19:03:49 -0400

    Added download options to Biodiversity and References

--- Wmis/ApiControllers/BioDiversityApiController.cs
+++ Wmis/ApiControllers/BioDiversityApiController.cs
@@ -14,9 +14,11 @@
 	using Dto;
 	using Models;
 
-	using Wmis.Auth;
+    using Wmis.Auth;
 	using Wmis.Logic;
 	using Wmis.WebApi;
+    using NPOI.HSSF.UserModel;
+
 
     /// <summary>
 	/// Bio Diversity API Controller
@@ -46,6 +48,199 @@
 		{
 			return Repository.BioDiversityGet(searchRequestParameters);
 		}
+
+        /// <summary>
+        /// Download list of Species depending on the filters passed to the diaplay list
+        /// 
+        /// </summary>
+        /// <param name="bioDiversityKey"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("download")]
+        public HttpResponseMessage Download([FromUri]BioDiversitySearchRequest sr)
+        {
+            var lstData = Repository.BioDiversityGet(sr);
+
+            var workbook = new HSSFWorkbook();
+            var sheet = workbook.CreateSheet("Species");
+
+            var header = sheet.CreateRow(0);
+            header.CreateCell(0).SetCellValue("Group");
+            header.CreateCell(1).SetCellValue("Kingdom");
+            header.CreateCell(2).SetCellValue("Order");
+            header.CreateCell(3).SetCellValue("Phylum");
+            header.CreateCell(4).SetCellValue("SubPhylum");
+            header.CreateCell(5).SetCellValue("Class");
+            header.CreateCell(6).SetCellValue("Family");
+            header.CreateCell(7).SetCellValue("Common Name");
+            header.CreateCell(8).SetCellValue("Name");
+            header.CreateCell(9).SetCellValue("Status Rank");
+            header.CreateCell(10).SetCellValue("Elcode");
+            header.CreateCell(11).SetCellValue("NonNTSpecies");
+            header.CreateCell(12).SetCellValue("Canada Known SubSpecies Count");
+            header.CreateCell(13).SetCellValue("Canada Known SubSpecies Description");
+            header.CreateCell(14).SetCellValue("NWT Known SubSpecies Count");
+            header.CreateCell(15).SetCellValue("NWT Known SubSpecies Description");
+            header.CreateCell(16).SetCellValue("Age of Maturity");
+            header.CreateCell(16).SetCellValue("Age of Maturity Description");
+            header.CreateCell(17).SetCellValue("Reproduction Frequency Per Year");
+            header.CreateCell(18).SetCellValue("Reproduction Frequency Per Year Description");
+            header.CreateCell(18).SetCellValue("Longevity");
+            header.CreateCell(19).SetCellValue("Longevity Description");
+            header.CreateCell(20).SetCellValue("Vegetation Reproduction Description");
+            header.CreateCell(21).SetCellValue("HostFish Description");
+            header.CreateCell(22).SetCellValue("Other Reproduction Description");
+            header.CreateCell(23).SetCellValue("Ecozone Description");
+            header.CreateCell(24).SetCellValue("Ecoregion Description");
+            header.CreateCell(25).SetCellValue("Protected Area Description");
+            header.CreateCell(26).SetCellValue("Range Extent Score");
+            header.CreateCell(27).SetCellValue("Range Extent Description");
+            header.CreateCell(28).SetCellValue("Distribution Percentage");
+            header.CreateCell(29).SetCellValue("Area Of Occupancy Score");
+            header.CreateCell(30).SetCellValue("Area Of Occupancy Description");
+            header.CreateCell(31).SetCellValue("Historical Distribution Description");
+            header.CreateCell(32).SetCellValue("Marine Distribution Description");
+            header.CreateCell(33).SetCellValue("Winter Distribution Description");
+            header.CreateCell(34).SetCellValue("Habitat Description");
+            header.CreateCell(35).SetCellValue("Environmental Specificity Score");
+            header.CreateCell(36).SetCellValue("Environmental Specificity Description");
+            header.CreateCell(37).SetCellValue("Population Size Score");
+            header.CreateCell(38).SetCellValue("Population Size Description");
+            header.CreateCell(39).SetCellValue("Number Of Occurences Score");
+            header.CreateCell(40).SetCellValue("Number Of Occurences Description");
+            header.CreateCell(41).SetCellValue("Density Description");
+            header.CreateCell(42).SetCellValue("Threats Score");
+            header.CreateCell(43).SetCellValue("Threats Description");
+            header.CreateCell(44).SetCellValue("Intrinsic Vulnerability Score");
+            header.CreateCell(45).SetCellValue("Intrinsic Vulnerability Description");
+            header.CreateCell(46).SetCellValue("Short Term Trends Score");
+            header.CreateCell(47).SetCellValue("Short Term Trends Description");
+            header.CreateCell(48).SetCellValue("Long Term Trends Score");
+            header.CreateCell(49).SetCellValue("Long Term Trends Description");
+            header.CreateCell(50).SetCellValue("Status RankDescription");
+            header.CreateCell(52).SetCellValue("SRank");
+            header.CreateCell(53).SetCellValue("Decision Process Description");
+            header.CreateCell(54).SetCellValue("Economic Status Description");
+            header.CreateCell(55).SetCellValue("Cosewic Status Description");
+            header.CreateCell(56).SetCellValue("Cosewic Status");
+            header.CreateCell(57).SetCellValue("NRank");
+            header.CreateCell(58).SetCellValue("Federal Species At Risk Status Description");
+            header.CreateCell(59).SetCellValue("NWT SARC Assessment Description");
+            header.CreateCell(60).SetCellValue("IUCN Status");
+            header.CreateCell(61).SetCellValue("IUCN Description");
+            header.CreateCell(62).SetCellValue("GRank");
+            header.CreateCell(63).SetCellValue("SARA Status");
+            header.CreateCell(64).SetCellValue("NWT Status Rank");
+            header.CreateCell(65).SetCellValue("Last Updated");
+
+            var rowIndex = 1;
+
+            foreach (var data in lstData.Data)
+            {
+                var row = sheet.CreateRow(rowIndex);
+                row.CreateCell(0).SetCellValue(data.Group.Name);
+                row.CreateCell(1).SetCellValue(data.Kingdom.Name);
+                row.CreateCell(2).SetCellValue(data.Order.Name);
+                row.CreateCell(3).SetCellValue(data.Phylum.Name);
+                row.CreateCell(4).SetCellValue(data.SubPhylum.Name);
+                row.CreateCell(5).SetCellValue(data.Class.Name);
+                row.CreateCell(6).SetCellValue(data.Family.Name);
+                row.CreateCell(7).SetCellValue(data.CommonName);
+                row.CreateCell(8).SetCellValue(data.Name);
+                row.CreateCell(9).SetCellValue(data.StatusRank.Name);
+                row.CreateCell(10).SetCellValue(data.Elcode);
+                row.CreateCell(11).SetCellValue(data.NonNtSpecies.ToString());
+                row.CreateCell(12).SetCellValue(data.CanadaKnownSubSpeciesCount.ToString());
+                row.CreateCell(13).SetCellValue(data.CanadaKnownSubSpeciesDescription);
+                row.CreateCell(14).SetCellValue(data.NwtKnownSubSpeciesCount.ToString());
+                row.CreateCell(15).SetCellValue(data.NwtKnownSubSpeciesDescription);
+                row.CreateCell(16).SetCellValue(data.AgeOfMaturity.ToString());
+                row.CreateCell(16).SetCellValue(data.AgeOfMaturityDescription);
+                row.CreateCell(17).SetCellValue(data.ReproductionFrequencyPerYear.ToString());
+                row.CreateCell(18).SetCellValue(data.ReproductionFrequencyPerYearDescription);
+                row.CreateCell(18).SetCellValue(data.Longevity.ToString());
+                row.CreateCell(19).SetCellValue(data.LongevityDescription);
+                row.CreateCell(20).SetCellValue(data.VegetationReproductionDescription);
+                row.CreateCell(21).SetCellValue(data.HostFishDescription);
+                row.CreateCell(22).SetCellValue(data.OtherReproductionDescription);
+                row.CreateCell(23).SetCellValue(data.EcozoneDescription);
+                row.CreateCell(24).SetCellValue(data.EcoregionDescription);
+                row.CreateCell(25).SetCellValue(data.ProtectedAreaDescription);
+                row.CreateCell(26).SetCellValue(data.RangeExtentScore);
+                row.CreateCell(27).SetCellValue(data.RangeExtentDescription);
+                row.CreateCell(28).SetCellValue(data.DistributionPercentage);
+                row.CreateCell(29).SetCellValue(data.AreaOfOccupancyScore);
+                row.CreateCell(30).SetCellValue(data.AreaOfOccupancyDescription);
+                row.CreateCell(31).SetCellValue(data.HistoricalDistributionDescription);
+                row.CreateCell(32).SetCellValue(data.MarineDistributionDescription);
+                row.CreateCell(33).SetCellValue(data.WinterDistributionDescription);
+                row.CreateCell(34).SetCellValue(data.HabitatDescription);
+                row.CreateCell(35).SetCellValue(data.EnvironmentalSpecificityScore);
+                row.CreateCell(36).SetCellValue(data.EnvironmentalSpecificityDescription);
+                row.CreateCell(37).SetCellValue(data.PopulationSizeScore);
+                row.CreateCell(38).SetCellValue(data.PopulationSizeDescription);
+                row.CreateCell(39).SetCellValue(data.NumberOfOccurencesScore);
+                row.CreateCell(40).SetCellValue(data.NumberOfOccurencesDescription);
+                row.CreateCell(41).SetCellValue(data.DensityDescription);
+                row.CreateCell(42).SetCellValue(data.ThreatsScore);
+                row.CreateCell(43).SetCellValue(data.ThreatsDescription);
+                row.CreateCell(44).SetCellValue(data.IntrinsicVulnerabilityScore);
+                row.CreateCell(45).SetCellValue(data.IntrinsicVulnerabilityDescription);
+                row.CreateCell(46).SetCellValue(data.ShortTermTrendsScore);
+                row.CreateCell(47).SetCellValue(data.ShortTermTrendsDescription);
+                row.CreateCell(48).SetCellValue(data.LongTermTrendsScore);
+                row.CreateCell(49).SetCellValue(data.LongTermTrendsDescription);
+                row.CreateCell(50).SetCellValue(data.StatusRankDescription);
+                row.CreateCell(52).SetCellValue(data.SRank);
+                row.CreateCell(53).SetCellValue(data.DecisionProcessDescription);
+                row.CreateCell(54).SetCellValue(data.EconomicStatusDescription);
+                row.CreateCell(55).SetCellValue(data.CosewicStatusDescription);
+                row.CreateCell(56).SetCellValue(data.CosewicStatus.Name);
+                row.CreateCell(57).SetCellValue(data.NRank);
+                row.CreateCell(58).SetCellValue(data.FederalSpeciesAtRiskStatusDescription);
+                row.CreateCell(59).SetCellValue(data.NwtsarcAssessmentDescription);
+                row.CreateCell(60).SetCellValue(data.IucnStatus);
+                row.CreateCell(61).SetCellValue(data.IucnDescription);
+                row.CreateCell(62).SetCellValue(data.GRank);
+                row.CreateCell(63).SetCellValue(data.SaraStatus.Name);
+                row.CreateCell(64).SetCellValue(data.NwtStatusRank.Name);
+                row.CreateCell(65).SetCellValue(data.LastUpdated.ToString());
+
+                rowIndex++;
+            }
+
+            var directoryName = Path.Combine(Path.GetTempPath(), "WMIS");
+            string strFile = "Species_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xls";
+            string fullPath = Path.Combine(directoryName, strFile);
+
+            if (!Directory.Exists(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+
+            using (var fileStream = System.IO.File.Create(fullPath))
+            {
+                workbook.Write(fileStream);
+            }
+
+            if (System.IO.File.Exists(fullPath))
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                var stream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+                response.Content = new StreamContent(stream);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = strFile
+                };
+
+                return response;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.NotFound);
+        }
+
+
 
 		[HttpGet]
 		[Route("{bioDiversityKey:int?}")]
--- Wmis/ApiControllers/ReferenceController.cs
+++ Wmis/ApiControllers/ReferenceController.cs
@@ -6,6 +6,12 @@
 	using Configuration;
 	using Models;
 
+    using NPOI.HSSF.UserModel;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.IO;
+    using System.Net;
+
     [RoutePrefix("api/references")]
 	public class ReferenceController : BaseApiController
     {
@@ -21,7 +27,79 @@
 			return Repository.ReferencesSearch(rr);
 		}
 
-		[HttpPost]
+        [HttpGet]
+        [Route("download")]
+        public HttpResponseMessage Download([FromUri]Dto.ReferenceRequest rr)
+        {
+            var lstData = Repository.ReferencesSearch(rr);
+
+            var workbook = new HSSFWorkbook();
+            var sheet = workbook.CreateSheet("References");
+            var header = sheet.CreateRow(0);
+            
+            header.CreateCell(0).SetCellValue("ID");
+            header.CreateCell(1).SetCellValue("Code");
+            header.CreateCell(2).SetCellValue("Author");
+            header.CreateCell(3).SetCellValue("Year");
+            header.CreateCell(4).SetCellValue("Title");
+            header.CreateCell(5).SetCellValue("Edition Publication Organization");
+            header.CreateCell(6).SetCellValue("Volume Page");
+            header.CreateCell(7).SetCellValue("Publisher");
+            header.CreateCell(8).SetCellValue("City");
+            header.CreateCell(9).SetCellValue("Location");
+
+            var rowIndex = 1;
+
+            foreach (var data in lstData.Data)
+            {
+                var row = sheet.CreateRow(rowIndex);
+                row.CreateCell(0).SetCellValue(data.Key);
+                row.CreateCell(1).SetCellValue(data.Code);
+                row.CreateCell(2).SetCellValue(data.Author);
+                row.CreateCell(3).SetCellValue(data.Year.ToString());
+                row.CreateCell(4).SetCellValue(data.Title);
+                row.CreateCell(5).SetCellValue(data.EditionPublicationOrganization);
+                row.CreateCell(6).SetCellValue(data.VolumePage);
+                row.CreateCell(7).SetCellValue(data.Publisher);
+                row.CreateCell(8).SetCellValue(data.City);
+                row.CreateCell(9).SetCellValue(data.Location);
+
+                rowIndex++;
+            }
+
+            var directoryName = Path.Combine(Path.GetTempPath(), "WMIS");
+            string strFile = "References_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xls";
+            string fullPath = Path.Combine(directoryName, strFile);
+
+            if (!Directory.Exists(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+
+            using (var fileStream = System.IO.File.Create(fullPath))
+            {
+                workbook.Write(fileStream);
+            }
+
+            if (System.IO.File.Exists(fullPath))
+            {
+                HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+                var stream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+                response.Content = new StreamContent(stream);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = strFile
+                };
+
+                return response;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.NotFound);
+
+        }
+
+        [HttpPost]
 		[Route]
 		public void SaveReference(Models.Reference r)
 		{
