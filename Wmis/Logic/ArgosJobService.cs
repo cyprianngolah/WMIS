91eef11008edb495ad78c18b0f70697b40106b31
cyprian_ngolah <cyprian_ngolah@gov.nt.ca>
2021-04-14 11:27:05 -0600

    Change edit to view in survey and user pages and limit csv data load to only last 14 days; change title of application data specialist

--- Wmis/Logic/ArgosJobService.cs
+++ Wmis/Logic/ArgosJobService.cs
@@ -235,7 +235,9 @@ namespace Wmis.Logic
                 var passes = new List<ArgosSatellitePass>();
                 var dataRows = new List<ArgosCollarData>();
 
-                foreach (var row in file.Rows.Where(r => string.IsNullOrEmpty(r.Error) && r.Timestamp.HasValue && r.Timestamp <= DateTime.Now && r.Timestamp > DateTime.MinValue))
+                /* Check to ensure that only data within the last 14 days is included in the dataset (r.Timestamp >= DateTime.Today.AddDays(-14) */
+
+                foreach (var row in file.Rows.Where(r => string.IsNullOrEmpty(r.Error) && r.Timestamp.HasValue && r.Timestamp >= DateTime.Today.AddDays(-14) &&  r.Timestamp <= DateTime.Now && r.Timestamp > DateTime.MinValue))
                 {
                     IridiumOutputFileRow rowAsIridium = row as IridiumOutputFileRow;
                     ArgosOutputFileRow rowAsArgos = row as ArgosOutputFileRow;
@@ -431,9 +433,9 @@ namespace Wmis.Logic
 
                                 _repository.ArgosPassUpdate(pass.Key, badPassStatus.Key, comment);
                             }*/
-                        }
+            }
 
-                        lastLocationDate = pass.LocationDate;
+            lastLocationDate = pass.LocationDate;
                         lastCoord = myCoord;
                     }
                 }
